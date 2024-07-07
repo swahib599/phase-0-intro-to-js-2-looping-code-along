54617c19318044364fbfe0e1b172fba48ebac1f8
Shaib Echesa Makokha <echesaswahib@gmail.com>
2024-07-07 11:35:34 +0300

    looping code

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solutions in this file
+function printBadges(names) {
+    for (let i = 0; i < names.length; i++) {
+      console.log(`Welcome ${names[i]}! You are employee #${i + 1}.`);
+    }
+    return names;
+  }
+  
+  function tailsNeverFails() {
+    let tailsCount = 0;
+  
+    while (Math.random() < 0.5) {
+      tailsCount++;
+    }
+  
+    return `You got ${tailsCount} tails in a row!`;
+  }
+  
+  function writeCards(names, event) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+      messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+  }
+  
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
+  
